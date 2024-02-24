cfccdb5a62140e4bfe07fdd6fc1dc7e48bf094b3
mshum345 <mitchell.shumway101@gmail.com>
2024-02-24 15:32:30 -0700

    updating app.py and web_detect.py to interact with each other.

--- web-server/web_detect.py
+++ web-server/web_detect.py
@@ -2,6 +2,7 @@ import csv
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import sys
 
 # Load .env variables
 load_dotenv()
@@ -17,13 +18,7 @@ dataset_name = "WELFake Dataset"
 subject = "US_politics"
 i = 0
 
-prompt = """
-    Tell me if the following twitter post is factual or dissinformation. Your answer will have 4 sections seperated by a ";". 
-    Section 1 - a one number response of either "1" if it is factual or "0" if it is dissinformation. 
-    Section 2 - a level from 1-12 on how confident you are that your answer from Section 1 is correct.
-    Section 3 - a level from 1-12 on how truthful the twitter post is. 
-    Section 4 - A explanation of why the post is fact or fake and why you gave the confidence level you did. Here is the twitter post: 
-"""
+prompt = sys.argv[1]
 
 # Configure Gemini API
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
