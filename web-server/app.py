079ccb2cdc366b294723b0deb060f8fcbbe2f9fe
Jordan Williams <williamsjordanb@gmail.com>
2024-02-28 10:08:42 -0700

    Updated web server functionality, and updated gitignore.

--- web-server/app.py
+++ web-server/app.py
@@ -1,54 +1,31 @@
-from flask import Flask, render_template, request
-import subprocess
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import os
+from flask import Flask, render_template, request, send_from_directory
+from web_detect import run_prompt
+import threading
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         prompt = request.form['prompt']
         email = request.form['email']
+        # TODO: Regex to confirm email is in email format. Reject if not.
+
+        # Create a thread and pass the function to it
+        thread = threading.Thread(target=run_prompt, args=(prompt,email,))
+
+        # Start the thread
+        thread.start()
         
-        # Call detect.py script with prompt
-        detect_script = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'web_detect.py')
-        subprocess.run(['python', detect_script, prompt])
-        
-        # Send CSV file as attachment via email
-        send_email(email)
-        
+    # TODO: Render a page that tells the user the thing is running, and that we will get back to them with an email when done.
     return render_template('index.html')
 
-def send_email(receiver_email):
-    sender_email = "your_sender_email@example.com"
-    sender_password = "your_sender_password"
-    
-    message = MIMEMultipart()
-    message['From'] = sender_email
-    message['To'] = receiver_email
-    message['Subject'] = "Disinformation Detection Results"
-    
-    filename = "results.csv"
-    attachment = open(filename, "rb")
-    
-    part = MIMEText("Please find the attached CSV file for Disinformation Detection results.")
-    message.attach(part)
-    
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    message.attach(part)
-    
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender_email, sender_password)
-    text = message.as_string()
-    server.sendmail(sender_email, receiver_email, text)
-    server.quit()
+# Define the route for downloading files
+@app.route('/download/<path:filename>',  methods=['GET'])
+def download(filename):
+    # Send the file to the user
+    return send_from_directory("dynamic/prompt_results", filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web-server/web_detect.py
+++ web-server/web_detect.py
@@ -3,146 +3,157 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import sys
-
-# Load .env variables
-load_dotenv()
-
-# Declare main variables
-script_directory = os.path.dirname(os.path.abspath(__file__))
-parent_directory = os.path.dirname(script_directory)
-datasets_directory = os.path.join(parent_directory, "Datasets")
-in_file = os.path.join(datasets_directory, "WELFake", "WELFake_Dataset_5000.csv")
-out_file = os.path.join(script_directory, "results.csv")
-data = []
-dataset_name = "WELFake Dataset"
-subject = "US_politics"
-i = 0
-
-prompt = sys.argv[1]
-
-# Configure Gemini API
-genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-model = genai.GenerativeModel(model_name='gemini-pro')
-
-# Pulls data in from csv file and organizes it in a list of dictionaries
-if in_file == "./example_data.csv":
-    with open(in_file, 'r') as in_csv: # For Example Dataset (not utf-8)
-        reader = csv.DictReader(in_csv)
-        in_data = list(reader)
-else:
-    with open(in_file, 'r', encoding='utf-8', errors='ignore') as in_csv: # For all datasets encoded in utf-8
-        reader = csv.DictReader(in_csv)
-        in_data = list(reader)
-
-# Establish headers
-headers = [
-    "id", "dataset", "text", "subject", "prompt", "label", "response", "confidence_level", "truth_level", "correct", "response_explanation"
-]
-
-# Write the header row
-with open(out_file, mode="a", newline="", encoding='utf-8') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(headers)
-
-# Loops through every row of data in the csv file
-for current in in_data:
-    i += 1
-    print(str(i) + ":")
-
-    try:
-
-        # For testing purposes
-        """
-        if i < 8749:
-            continue
-        """
-        if i > 5000:
-            break
-        
-        # Add text to current prompt and strips text of any ";"
-        full_prompt = prompt + current["text"].replace(";", "")
-
-        # Interact with Gemini to fill out response, response_explanation, confidence, truthful_level, and correct
-        res = model.generate_content(
-        contents=full_prompt,
-        generation_config={
-        'temperature': 0,
-        'max_output_tokens': 800
-        },
-        safety_settings=[
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_NONE",
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_NONE",
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_NONE",
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS",
-            "threshold": "BLOCK_NONE",
-        },
-        ])
-
-        # Checks if the prompt was blocked
-        if str(res.prompt_feedback).replace("\n", "") == "block_reason: OTHER":
-            print(str(i) + ": Prompt Blocked")
-            continue
-
-        # Checks for if response is empty
-        if res.parts == []:
-            print(str(i) + ": Response is empty")
-            continue
-
-        #print(res.text)
-        res = res.text.split(";")
-
-        # Checks if response is correct length
-        if len(res) <= 1:
-            print("Prompt did not return correct response")
-            continue
-        
-        # Determines if PaLM 2 was correct or not
-        if current["label"] == res[0]:
-            correct = 1
-        else:
-            correct = 0
-
-        # Create the new row of data for output csv
-        new_list = []
-        new_list.append(i) # id
-        new_list.append(dataset_name) # dataset
-        new_list.append(current["text"]) # text
-        new_list.append(subject) # subject
-        new_list.append(prompt.replace("\n", "")) # prompt
-        new_list.append(current["label"]) # label
-        new_list.append(res[0]) # response
-        new_list.append(res[1]) # confidence_level
-        new_list.append(res[2]) # truth_level
-        new_list.append(correct) # correct
-        new_list.append(res[3]) # response_explanation
-
-        data.append(new_list)
-
-        if i % 50 == 0:
-            with open(out_file, mode="a", newline="", encoding='utf-8') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerows(data)
-            
-            # Clear the data list after writing to the CSV file
-            data = []
-
-    except Exception as e:
-        print(f"(server error) Exception: {str(e)}")
-        continue  # Move on to the next iteration
+import uuid
+from helper_functions.email_functions import send_email
 
 
-# Write the data to the out_file csv
-if data:
+def run_prompt(prompt, email):
+    # Load .env variables
+    load_dotenv()
+
+    # Declare main variables
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    parent_directory = os.path.dirname(script_directory)
+    datasets_directory = os.path.join(parent_directory, "Datasets")
+    in_file = os.path.join(datasets_directory, "WELFake", "WELFake_Dataset_5000.csv")
+    out_file_name = str(uuid.uuid4()) + ".csv"
+    out_file = os.path.join(script_directory, "dynamic", "prompt_results", out_file_name)
+    file_download_path = os.path.join("downloads", out_file_name) # This will allow us to send an email with the download path
+    data = []
+    dataset_name = "WELFake Dataset"
+    subject = "US_politics"
+    i = 0
+
+    # prompt = sys.argv[1]
+
+    # Configure Gemini API
+    genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+    model = genai.GenerativeModel(model_name='gemini-pro')
+
+    # Pulls data in from csv file and organizes it in a list of dictionaries
+    if in_file == "./example_data.csv":
+        with open(in_file, 'r') as in_csv: # For Example Dataset (not utf-8)
+            reader = csv.DictReader(in_csv)
+            in_data = list(reader)
+    else:
+        with open(in_file, 'r', encoding='utf-8', errors='ignore') as in_csv: # For all datasets encoded in utf-8
+            reader = csv.DictReader(in_csv)
+            in_data = list(reader)
+
+    # Establish headers
+    headers = [
+        "id", "dataset", "text", "subject", "prompt", "label", "response", "confidence_level", "truth_level", "correct", "response_explanation"
+    ]
+
+    # Write the header row
     with open(out_file, mode="a", newline="", encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerows(data)
+        csv_writer.writerow(headers)
+
+    # Loops through every row of data in the csv file
+    for current in in_data:
+        
+        i += 1
+        print(str(i) + ":")
+
+        try:
+
+            # For testing purposes
+            """
+            if i < 8749:
+                continue
+            """
+            if i > 5000:
+                break
+            
+            # Add text to current prompt and strips text of any ";"
+            full_prompt = prompt + current["text"].replace(";", "")
+
+            # Interact with Gemini to fill out response, response_explanation, confidence, truthful_level, and correct
+            res = model.generate_content(
+            contents=full_prompt,
+            generation_config={
+            'temperature': 0,
+            'max_output_tokens': 800
+            },
+            safety_settings=[
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_NONE",
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS",
+                "threshold": "BLOCK_NONE",
+            },
+            ])
+
+            # Checks if the prompt was blocked
+            if str(res.prompt_feedback).replace("\n", "") == "block_reason: OTHER":
+                print(str(i) + ": Prompt Blocked")
+                continue
+
+            # Checks for if response is empty
+            if res.parts == []:
+                print(str(i) + ": Response is empty")
+                continue
+
+            #print(res.text)
+            res = res.text.split(";")
+
+            # Checks if response is correct length
+            if len(res) <= 1:
+                print("Prompt did not return correct response")
+                continue
+            
+            # Determines if PaLM 2 was correct or not
+            if current["label"] == res[0]:
+                correct = 1
+            else:
+                correct = 0
+
+            # Create the new row of data for output csv
+            new_list = []
+            new_list.append(i) # id
+            new_list.append(dataset_name) # dataset
+            new_list.append(current["text"]) # text
+            new_list.append(subject) # subject
+            new_list.append(prompt.replace("\n", "")) # prompt
+            new_list.append(current["label"]) # label
+            new_list.append(res[0]) # response
+            new_list.append(res[1]) # confidence_level
+            new_list.append(res[2]) # truth_level
+            new_list.append(correct) # correct
+            new_list.append(res[3]) # response_explanation
+
+            data.append(new_list)
+
+            if i % 10 == 0:
+                with open(out_file, mode="a", newline="", encoding='utf-8') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerows(data)
+                
+                # Clear the data list after writing to the CSV file
+                data = []
+
+        except Exception as e:
+            print(f"(server error) Exception: {str(e)}")
+            continue  # Move on to the next iteration
+
+
+    # Write the data to the out_file csv
+    if data:
+        with open(out_file, mode="a", newline="", encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerows(data)
+
+
+    # Send CSV file as attachment via email
+    send_email(email, file_download_path)
