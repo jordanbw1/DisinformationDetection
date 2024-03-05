3ae2f7a64fb39c29f2b0e58f80c25e3a455b6ed0
Jordan Williams <williamsjordanb@gmail.com>
2024-03-05 16:05:31 -0700

    Added navbar to website

--- web-server/routes/documents.py
+++ web-server/routes/documents.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 documents = Blueprint('documents', __name__, template_folder='documents')
 
-@documents.route('/get-api-key')
+@documents.route('/get-key')
 def get_api_key():
     return render_template("get_api_key.html")
