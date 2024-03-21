2e0173efc7881282dfde80aa2bfdfb47f64c4b4c
Jordan Williams <williamsjordanb@gmail.com>
2024-03-21 11:27:45 -0600

    Changed nav bar more. Also enabled debug for flask dev

--- web-server/main.py
+++ web-server/main.py
@@ -295,4 +295,4 @@ def confirmation():
     return render_template("confirmation.html", prompt=prompt)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
