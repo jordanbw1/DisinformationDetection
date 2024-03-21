60a83ab3ec98e628f15484b820cec869e13ae709
Jordan Williams <williamsjordanb@gmail.com>
2024-03-21 14:54:40 -0600

    Further improved CSS.

--- web-server/helper_functions/prompt.py
+++ web-server/helper_functions/prompt.py
@@ -1,4 +1,9 @@
 def append_instructions(prompt):
-    instructions = """\nYour answer will have 4 sections seperated by a ";".      Section 1 - a one number response of either "1" if it is factual or "0" if it is dissinformation.      Section 2 - a level from 1-12 on how confident you are that your answer from Section 1 is correct.     Section 3 - a level from 1-12 on how truthful the twitter post is.      Section 4 - An explanation of why the post is fact or fake and why you gave the confidence level you did. Here is the twitter post:"""
+    instructions = """\nYour answer will have 4 sections seperated by a ";".
+Section 1 - a one number response of either "1" if it is factual or "0" if it is dissinformation.
+Section 2 - a level from 1-12 on how confident you are that your answer from Section 1 is correct.
+Section 3 - a level from 1-12 on how truthful the twitter post is.
+Section 4 - An explanation of why the post is fact or fake and why you gave the confidence level you did.
+Here is the twitter post:"""
     fixed_prompt = str(prompt) + instructions
     return fixed_prompt
