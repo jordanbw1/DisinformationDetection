fc1e33000e7ed484d9f3a4e80f10e7dfd3320bf6
Jordan Williams <williamsjordanb@gmail.com>
2024-05-06 13:43:49 -0600

    Cleaned up styling and font for all pages.

--- web-server/routes/documents.py
+++ web-server/routes/documents.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 documents = Blueprint('documents', __name__, template_folder='documents')
 
-@documents.route('/get-key')
-def get_api_key():
-    return render_template("get_api_key.html")
+@documents.route('/get-gemini-key')
+def get_gemini_key():
+    return render_template("get_gemini_key.html")
+
+@documents.route('/get-chatgpt-key')
+def get_chatgpt_key():
+    return render_template("get_chatgpt_key.html")
